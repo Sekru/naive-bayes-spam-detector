a124a0241a7021a2d8e2bee6ddafcf0b4be45006
Sekru <rusekpawel1@gmail.com>
2019-03-27 20:39:01 +0100

    spam detector with database

--- main.py
+++ main.py
@@ -1,61 +1,57 @@
 import re
 from functools import reduce
-
-trainingData = [
-    {"message": "Interesujacy profil !!! Wiktoria . Jesli chcesz dotrzec do jeszcze wiekszej ilosci osob zapraszamy do wspolpracy www.instapromowanie.pl", "label": "spam"},
-    {"message": "Piekny profil zapraszam do mnie w wolnej chwili", "label": "spam"},
-    {"message": "Jak bede kupowac droższa palete do konturowania to na pewno bedzie to smashbox", "label": "ham"},
-    {"message": "Mnie sie lepiej sprawdzila seria rożana, ale te też daja rade", "label": "ham"},
-    {"message": "Ooo mialam w reku ten krem , ciekawe jak sie sprawdzi", "label": "ham"},
-    {"message": "Jest super zapraszam także do mnie", "label": "spam"},
-    {"message": "great post!", "label": "spam"},
-    {"message": "Hmm zaintrygowalas mnie, koniecznie musze wyprobowac ", "label": "ham"},
-    {"message": "Sprzedam takie kalendarze wiecej info na priv", "label": "spam"},
-    {"message": "swietny profil i zdjecia uczieszylabym sie jeżeli bys tez zajrzala na moj profi", "label": "spam"},
-    {"message": "Wow, wyglada pieknie. Chyba czas odlożyc troche pieniedzy", "label": "ham"},
-    {"message": "Kochana super profil!! W wolnej chwili zapraszam do siebie", "label": "spam"},
-    {"message": "Oj tak mam ich mnostwo", "label": "ham"},
-    {"message": "I like for my art? Follow me!", "label": "spam"},
-    {"message": "Super by bylo gdyby kazdy nie byl otwarty i wymacany kiedy producenci znajda na to myk", "label": "ham"},
-    {"message": "kupilam te same pedzle rownież z mysla że do rozswietlacza bedzie jak znalazl do bronzera", "label": "ham"},
-    {"message": "Ja też upolowalam pedzle z biedry mam ten do pudru, rożu i rozswietlacza", "label": "ham"},
-    {"message": "Dopiero wczoraj udalo mi sie je kupic w naturze ciagle na polkach byly braki", "label": "ham"},
-    {"message": "Kupilam wczoraj krem z tej serii z Bielenda i jestem zachwycona", "label": "ham"},
-    {"message": "super profil w wolnej chwili zapraszam do mnie", "label": "spam"},
-    {"message": "Piekny profil zapraszam do mnie w wolnej chwili", "label": "spam"},
-]
+import csv
+import time
+import json
 
 def countWords(counter, words):
     for word in words:
-        counter[word] = counter.get(word, 0) + 1
+        if word != "subject":
+            counter[word] = counter.get(word, 0) + 1
 
 def preprocess_string(words):
     cleaned = re.sub('[^a-z\s]+', ' ', words,flags=re.IGNORECASE)
     cleaned = re.sub(r'\b\w{1,3}\b', '', cleaned)
-    cleaned = re.sub('(\s+)', ' ', cleaned)
     cleaned = cleaned.lower()
-    
+    cleaned = re.sub('(\s+)', ' ', cleaned)
     return cleaned.split(' ')
+
+def extractType(wordsString):
+    try:
+        return wordsString[1]
+    except TypeError:
+        pass
 
 def train(trainingData):
     spamWords = {}
     hamWords = {}
     totalSpam = 0
     totalHam = 0
+    start = time.time()
+    end = time.time()
     for s in range(len(trainingData)):
-        if trainingData[s]["label"] == "spam":
+        category = extractType(trainingData[s])
+
+        if category == '1':
             totalSpam = totalSpam + 1
-            spamWordsArray = preprocess_string(trainingData[s]["message"])
+            spamWordsArray = preprocess_string(trainingData[s][0])
             countWords(spamWords, spamWordsArray)
 
-        if trainingData[s]["label"] == "ham":
+        if category == '0':
             totalHam = totalHam + 1
-            hamWordsArray = preprocess_string(trainingData[s]["message"])
+            hamWordsArray = preprocess_string(trainingData[s][0])
             countWords(hamWords, hamWordsArray)
-
+    fh = open('hamWords.json','w')
+    fh.write(json.dumps(hamWords))
+    fh.close()
+    fs = open('spamWords.json','w')
+    fs.write(json.dumps(spamWords))
+    fs.close()
+    end = time.time()
+    print(str(end - start) + "sec")
     return spamWords, hamWords, totalSpam, totalHam
 
-def think(wordsString, pSpam, pHam, spamWords, hamWords):
+def think(wordsString, totalSpam, totalHam, spamWords, hamWords):
     pSpam = totalSpam / totalCounter
     pHam = totalHam / totalCounter
     pSWords = []
@@ -83,8 +79,8 @@ def think(wordsString, pSpam, pHam, spamWords, hamWords):
         pSWords.append(pSW)
         pHWords.append(pHW)
 
-    pSResult = reduce(lambda x, y: x * y, pSWords)
-    pHResult = reduce(lambda x, y: x * y, pHWords)
+    pSResult = reduce(lambda x, y: x * y, pSWords) * pSW
+    pHResult = reduce(lambda x, y: x * y, pHWords) * pHW
 
     if (pSResult > pHResult):
         print("Spam")
@@ -94,8 +90,10 @@ def think(wordsString, pSpam, pHam, spamWords, hamWords):
         print("Hard to say...")
     
 if __name__ == "__main__":
-    spamWords, hamWords, totalSpam, totalHam = train(trainingData)
-    totalCounter = len(trainingData)
+    with open('emails.csv', newline='') as csvfile:
+        emailsData = list(csv.reader(csvfile))
+    spamWords, hamWords, totalSpam, totalHam = train(emailsData)
+    totalCounter = len(emailsData)
     while True:
         something = input("Please enter something: ")
         think(something, totalSpam, totalHam, spamWords, hamWords)
